d6db5404e75204e766f03dfdffb4dc039ffc85ec
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-18 10:55:49 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -11,7 +11,7 @@ Special Thanks to <b> @ViruZs , @TheUnusualPsychopath </b>
 <b> Created by @anonymous9329 </b>"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "Hi 🤪"
+    UPGRADE_TEXT = "Hi 🤪 there is no upgrade plane till now ot may add in future"
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
