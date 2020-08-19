541d06232e61d1b1414ec96a131567e55e4fe7ad
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-19 14:01:25 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -43,11 +43,11 @@ async def progress_for_pyrogram(
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "[{0}{1}] \nP: {2}%\n".format(
-            ''.join(["▪️" for i in range(math.floor(percentage / 5))]),
-            ''.join(["◻" for i in range(20 - math.floor(percentage / 5))]),
+            ''.join(["🙇‍♂️" for i in range(math.floor(percentage / 5))]),
+            ''.join(["🙋‍♂️" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\nSpeed ☄: {2}/s\estimated total time ⏰: {3}\n".format(
+        tmp = progress + "{0} of {1}\nSpeed ☄: {2}/s\nestimated total time ⏰: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
