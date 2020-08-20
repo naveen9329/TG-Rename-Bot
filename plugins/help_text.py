b5f5dd133b12562a8af5e1e1192ac067ab0f14df
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-20 10:05:54 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 import os
 import sqlite3
 rom telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
+
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
