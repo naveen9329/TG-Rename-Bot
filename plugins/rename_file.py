6c2ee7e1523cd1fb7e6011d8ac18cedcf7733ec8
PROFESSOR <66514907+Adithyan1133-ctrl@users.noreply.github.com>
2020-08-27 13:02:00 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -43,13 +43,7 @@ async def rename_doc(bot, update):
         )
         return
     TRChatBase(update.from_user.id, update.text, "rename")
-    user_details=bot.get_chat_member("@Zed1Projctz", update.from_user.id)
-    try:
-        user_details
-    except:
-        print("Join")
-       
-    if (" " in update.text) and (update.reply_to_message is not None):
+      if (" " in update.text) and (update.reply_to_message is not None):
         cmd, file_name = update.text.split(" ", 1)
         if len(file_name) > 64:
             await update.reply_text(
