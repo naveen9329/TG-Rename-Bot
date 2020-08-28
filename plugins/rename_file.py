358cf33f42ca9d57f65ea1a37d087831b2091bf2
PROFESSOR <66514907+Adithyan1133-ctrl@users.noreply.github.com>
2020-08-28 07:45:30 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -41,7 +41,7 @@ async def rename_doc(bot, update):
             message_ids=update.message_id,
             revoke=True
         )
-         return
+        return
     TRChatBase(update.from_user.id, update.text, "rename")
     if (" " in update.text) and (update.reply_to_message is not None):
         cmd, file_name = update.text.split(" ", 1)
