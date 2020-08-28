5b036c5829b73aa2a6283bc2970b49bb62ce8c74
PROFESSOR <66514907+Adithyan1133-ctrl@users.noreply.github.com>
2020-08-28 07:20:22 +0530

    Update get_chat_member.py

--- plugins/get_chat_member.py
+++ plugins/get_chat_member.py
@@ -12,17 +12,8 @@ if bool(os.environ.get("WEBHOOK", False)):
 else:
     from config import Config
 
-# the Strings used for this "thing"
-from translation import Translation
-
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-from pyrogram import Client, Filters
-
-from helper_funcs.chat_base import TRChatBase
-from helper_funcs.display_progress import progress_for_pyrogram
-
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-# https://stackoverflow.com/a/37631799/4723940
-from PIL import Image
+from pyrogram import Client, Filters, ChatPermissions
+from pyrogram import InlineKeyboardMarkup, InlineKeyboardButton
+import pyrogram.errors
