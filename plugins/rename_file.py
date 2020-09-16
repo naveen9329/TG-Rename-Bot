55d8e2679fa8ac384e499f8206a02c5734322afb
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-09-16 14:44:01 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -134,7 +134,7 @@ async def rename_doc(bot, update):
     else:
         await bot.send_message(
             chat_id=update.chat.id,
-            text=Translation.REPLY_TO_DOC_FOR_RENAME_FILE,
+            text='time={}'.format(stop_time),
             reply_to_message_id=update.message_id
         )
 
