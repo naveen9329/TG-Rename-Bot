1df0198a40d57718dcdca551b6001443bbd34f9e
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-09-16 16:32:23 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -148,6 +148,14 @@ async def rename_doc(bot, update):
 @pyrogram.Client.on_message(pyrogram.Filters.command(["showthumbnail"]))
 async def show_thumbnail(bot, update):
     thumb_image = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+    if not os.path.exists(thumb_image_path):
+       mes = await get_thumb(update.from_user.id)
+       if mes != None:
+            m = await bot.get_messages(update.chat.id, mes.msg_id)
+            await m.download(file_name=thumb_image_path)
+            thumb_image_path = thumb_image_path
+       else:
+          thumb_image_path = None
     if update.from_user.id in Config.BANNED_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
@@ -157,7 +165,7 @@ async def show_thumbnail(bot, update):
         return
     if update.from_user.id not in Config.BANNED_USERS:
        if thumb_image is not None:
-         await bot.send_photo(chat_id=update.chat.id, photo=thumb_image)
+         await bot.send_photo(chat_id=update.chat.id, photo=thumb_image)            
        if thumb_image is None:
          await update.reply_text("No thumbnail found 🤷‍♂️")
 
