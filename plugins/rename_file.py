5716806594a03d3d275a71401229aca4a071a29b
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-09-16 18:36:38 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -146,19 +146,3 @@ async def rename_doc(bot, update):
         )
         return thumb_image_path
     
-@pyrogram.Client.on_message(pyrogram.Filters.command(["showthumbnail"]))
-async def show_thumbnail(bot, update):
-    global thumb_image_path
-    if update.from_user.id in Config.BANNED_USERS:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
-        return
-    if update.from_user.id not in Config.BANNED_USERS:
-       if thumb_image_path is not None:
-         await bot.send_photo(chat_id=update.chat.id, photo=thumb_image_path)            
-       if thumb_image is None:
-         await update.reply_text("No thumbnail found 🤷‍♂️")
-
