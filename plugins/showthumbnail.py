9126e7f44f974223b1048a4cf1f61cf84450da46
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-09-16 18:38:26 +0530

    Update showthumbnail.py

--- plugins/showthumbnail.py
+++ plugins/showthumbnail.py
@@ -1,6 +1,12 @@
 @pyrogram.Client.on_message(pyrogram.Filters.command(["showthumbnail"]))
 async def show_thumbnail(bot, update):
-    global thumb_image_path
+    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+    if not os.path.exists(thumb_image_path):
+       mes = await get_thumb(update.from_user.id)
+       if mes != None:
+          m = await bot.get_messages(update.chat.id, mes.msg_id)
+          await m.download(file_name=thumb_image_path)
+       thumb_image_path = thumb_image_path
     if update.from_user.id in Config.BANNED_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
